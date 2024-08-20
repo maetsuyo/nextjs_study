63f35d8cf258d1bb9a9633b36dbaa49943078927
maetsuyo <maenoorehatuyokatta8009@gmail.com>
2024-08-21 03:27:02 +0900

    first commit

---
+++ src/app/postAction.ts
@@ -0,0 +1,16 @@
+"use server";
+import usersData from "./data/users.json";
+
+export async function postAction(formData:FormData) {
+    const id = formData.get("id");
+    const pass = formData.get("pass");
+    const user = usersData.users.find(user => user.id === id && user.pass === pass);
+
+    console.log(id);
+    console.log(pass);
+    if (user) {
+        console.log("ログイン成功");
+    } else {
+        console.log("ログイン失敗");
+    }
+}
