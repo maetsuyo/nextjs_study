bde54a93459b6b0f00d111b50bfd469025213c58
maetsuyo <maenoorehatuyokatta8009@gmail.com>
2024-08-21 12:10:59 +0900

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 import { postAction } from "./postAction";
 
 export default function Home() {
   return (
-    <form action={postAction}>
-      <input type="text" name="id"/>
-      <input type="text" name="pass"/>
-      <button type="submit">送信</button>
-    </form>
+    <div>
+      <form action={postAction}>
+        <input type="text" name="id"/>
+        <input type="text" name="pass"/>
+        <button type="submit">送信</button>
+      </form>
+    </div>
+    <div>
+      <ul>
+        <li>
+          <Link href="/users/1">user1</Link>
+        </li>
+        <li>
+          <Link href="/users2/">user2</Link>
+        </li>
+        <li>
+          <Link href="/users/3">user3</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
---
+++ src/app/users/[userId]/page.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    params : {userId: string};
+}
+
+export default function UserPage(props:Props) {
+    return (
+        <>
+            <h1 className="text-lg border-b pb-1 mb-1">
+                User Page { props.params.userId}
+            </h1>
+        </>
+    )
+};
